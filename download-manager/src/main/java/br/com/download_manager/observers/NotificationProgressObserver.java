9aafb49fc4bf919315325a5a42e22b820bd65f8c
Infra <vjviana@thoughtworks.com>
2018-11-29 17:23:23 -0300

    Refactoring download manager

--- download-manager/src/main/java/br/com/download_manager/observers/NotificationProgressObserver.java
+++ download-manager/src/main/java/br/com/download_manager/observers/NotificationProgressObserver.java
@@ -3,6 +3,7 @@ package br.com.download_manager.observers;
 import android.content.Context;
 
 import br.com.download_manager.model.DownloadManagerMessage;
+import br.com.download_manager.utils.ResourceMessages;
 
 public class NotificationProgressObserver extends NotificationObserver {
 
@@ -12,7 +13,9 @@ public class NotificationProgressObserver extends NotificationObserver {
 
     @Override
     public void update(DownloadManagerMessage message) {
-        builder.setProgress(100, message.getProgress(), false);
+        builder.setProgress(100, message.getProgress(), false)
+                .setContentTitle(String.format(ResourceMessages.NOTIFICATION_DOWNLOADING_TITLE, message.getFileName()));
+
         notificationManager.notify(message.getNotificationId(), builder.build());
     }
 }
